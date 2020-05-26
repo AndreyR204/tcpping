4eb86b4e38d025bb8a216aefb234a2b20cc50d14
AndreyR204 <andrejrybenok@gmail.com>
2020-05-26 19:24:07 +0500

    Change app.py, main.py, requirements.txt

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ Utility for ping via TCP
 git clone 
 pip install -r requirements.txt
 ## Usage
-python3 main.py host [port] [maxCount]
+python main.py  [-h] [--host HOST] [--port PORT] [--max MAX]
 
-python main.py host [port] [maxCount]
-
-python main.py 192.168.0.1
-Connected to 192.168.0.1[80]: tcp_seq=0 time=12.75 ms
-Connected to 192.168.0.1[80]: tcp_seq=1 time=19.48 ms
-Connected to 192.168.0.1[80]: tcp_seq=2 time=19.65 ms
+>python main.py -H 192.168.0.1
+>Connected to 192.168.0.1[80]: tcp_seq=0 time=2.18 ms 
+>Connected to 192.168.0.1[80]: tcp_seq=1 time=1.50 ms
+>Connected to 192.168.0.1[80]: tcp_seq=2 time=11.54 ms
 
 Ping Results: Connections (Total/Pass/Fail): [3/3/0] (Failed: 0%)
--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 from tcping import app
 from tcping import variables
 
-host = None
-port = 80
-
-# Default to 10000 connections max
-maxcount = 10000
-
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', '-H', default='127.0.0.1', help='host')
+parser.add_argument('--port', '-p', default=80, help='port')
+parser.add_argument('--max', '-m', default=100, help='max count')
+args = parser.parse_args()
 # Host
 try:
-    host = sys.argv[1]
+    host = args.host
 except IndexError:
     print("Usage: tcpping.py host [port] [maxCount]")
     sys.exit(1)
 
 # Port
 try:
-    port = int(sys.argv[2])
+    port = args.port
 except ValueError:
-    print("Error: Port Must be Integer:", sys.argv[3])
+    print("Error: Port Must be Integer:", args.port)
     sys.exit(1)
 except IndexError:
     pass
 
 # maxCount
 try:
-    maxcount = int(sys.argv[3])
+    maxcount = int(args.max)
 except ValueError:
-    print("Error: Max Count Value Must be Integer", sys.argv[3])
+    print("Error: Max Count Value Must be Integer", args.max)
     sys.exit(1)
 except IndexError:
     pass
-app.ping_hosts(host, port, maxcount)
+app.ping_hosts(args.host, args.port, args.max)
 app.get_results(variables.count, variables.passed, variables.failed)
