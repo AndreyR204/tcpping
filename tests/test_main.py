c5aad9757e2ee9cf5f5bf3f21b561a23fd18d3ca
AndreyR204 <andrejrybenok@gmail.com>
2020-05-26 22:49:13 +0500

    Change app.py, main.py, requirements.txt

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,1 @@
-import unittest
-import main
 
-
-class TestStringMethods(unittest.TestCase):
-
-    def test_variables(self):
-        self.assertEqual(main.host, main.args.host)
-
-
-if __name__ == '__main__':
-    unittest.main()
